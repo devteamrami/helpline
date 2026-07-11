b32a716fb1f12d2d3142d322e7ce6ee3bb9ac11e
rakeshghosh2023 <145310650+rakeshghosh2023@users.noreply.github.com>
2026-07-11 21:33:35 +0530

    en

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://api.ramiscope.com/api/v1', // Update with your production API URL
+  apiUrl: 'https://helplineapi.ramiscope.com/api/v1', // Update with your production API URL
   apiTimeout: 30000,
   tokenKey: 'rpms_access_token',
   refreshTokenKey: 'rpms_refresh_token',
